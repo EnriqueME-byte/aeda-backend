dd8e76a3d6e44a76bb3f976f4d38da838bffc680
EnriqueME-byte <rapredat@gmail.com>
2023-04-16 12:05:02 -0600

    change

--- src/main/java/sakicorp/SakicorpApplication.java
+++ src/main/java/sakicorp/SakicorpApplication.java
@@ -1,5 +1,5 @@
-package sakicorp;
 
+package sakicorp;
 import java.time.LocalDateTime;
 
 import org.springframework.boot.SpringApplication;
@@ -8,9 +8,58 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SakicorpApplication {
 
+	public static String sinNombre = "HOLA";
 	public static void main(String[] args) {
 		LocalDateTime datetime = LocalDateTime.now();
 		System.out.println(datetime);
+		
+		sinNombre = "PERRO";
+		System.out.println(sinNombre);
+		
+		long k = 5L;
+		int j = 6;
+
+		
+		Long c = new Long(2);
+		
+		switch(j) {
+			case 2:
+				System.out.println("hola");
+			case 2*3:
+				System.out.println("dos");
+			default:
+				System.out.println("default");
+				break;
+ 				
+		}
+		if(k == j) {
+			System.out.println("iguales");
+		}
+		int i = 1;
+		do {
+			i++;
+			System.out.println("hola"+i);
+		
+		}while(i < 1);
+		
+		try {
+			try {
+				
+			}catch(Exception e) {
+				
+			}
+			finally {
+				
+			}
+		}catch(Exception e) {
+			
+		}
+		finally{
+			
+		}
+		
+		
+		
 		SpringApplication.run(SakicorpApplication.class, args);
 	}
 
